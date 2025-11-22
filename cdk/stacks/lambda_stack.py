7c0a03982a8d1a5122504596ba607dcb68ed9e28
Puru Vyas <87336018+1NFY@users.noreply.github.com>
2025-11-22 20:59:43 +0530

    Update lambda_stack.py

---
+++ cdk/stacks/lambda_stack.py
@@ -0,0 +1,36 @@
+import os 
+from aws_cdk import (
+    Stack,
+    aws_iam as iam,
+    aws_lambda as _lambda,
+    Duration as _duration
+)
+
+
+class LambdaStack(Stack):
+    def __init__(
+            self,
+            scope : Construct,
+            construct_id : str,
+            *,
+            env : Environment,
+            **kwargs,
+    ) -> None:
+        super().__init__(scope, construct_id, env = env, **kwargs)
+    
+    root_dir = os.path.dirname(os.path.abspath('README.md'))
+    lambdarole = iam.role.from_role_name("lambdarole")
+
+    data_clean_lambda = _lambda.Function(
+        self,
+        "DataCleanLambda",
+        runtime = _lambda.Runtime.PYTHON_3_12,
+        function_name = "data_clean_lambda",
+        code = _lambda.Code.from_asset(
+            f"{root_dir}/lambda/data-clean"
+        ),
+        handler = 'app.lambda_handler',
+        role = lambdarole,
+        timeout = _duration.minutes(1)
+    )
+    
