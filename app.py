a37eba618fc3181afa7c8a6b0e1019341a5575f7
Puru Vyas <87336018+1NFY@users.noreply.github.com>
2025-11-22 21:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,19 +10,19 @@ deploy_env = Environment(
 )
 project_synth = DefaultStackSynthesizer(
     # ARN of the role assumed by the CLI and Pipeline to deploy here
-            deploy_role_arn= f"arn:aws:iam::{env.AWS_ACCOUNT_ID}:role/cdk-hnb659fds-deploy-role-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
+            deploy_role_arn= f"arn:aws:iam::{account_id}:role/cdk-hnb659fds-deploy-role-{account_id}-ap-northeast-1",
             # Name of the S3 bucket for file assets
-            file_assets_bucket_name= f"cdk-hnb659fds-assets-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
+            file_assets_bucket_name= f"cdk-hnb659fds-assets-{account_id}-ap-northeast-1",
             bucket_prefix="cdk",
             # ARN of the role used for file asset publishing (assumed from the CLI role)
-            file_asset_publishing_role_arn=f"arn:aws:iam::{env.AWS_ACCOUNT_ID}:role/cdk-hnb659fds-file-publishing-role-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
+            file_asset_publishing_role_arn=f"arn:aws:iam::{account_id}:role/cdk-hnb659fds-file-publishing-role-{account_id}-ap-northeast-1",
             # ARN of the role used for Docker asset publishing (assumed from the CLI role)
-            image_asset_publishing_role_arn=f"arn:aws:iam::{env.AWS_ACCOUNT_ID}:role/cdk-hnb659fds-image-publishing-role-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
-            image_assets_repository_name=f"cdk-hnb659fds-container-assets-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
+            image_asset_publishing_role_arn=f"arn:aws:iam::{account_id}:role/cdk-hnb659fds-image-publishing-role-{account_id}-ap-northeast-1",
+            image_assets_repository_name=f"cdk-hnb659fds-container-assets-{account_id}-ap-northeast-1",
             # ARN of the role passed to CloudFormation to execute the deployments
-            cloud_formation_execution_role=f"arn:aws:iam::{env.AWS_ACCOUNT_ID}:role/cdk-hnb659fds-cfn-exec-role-{env.AWS_ACCOUNT_ID}-ap-northeast-1",
+            cloud_formation_execution_role=f"arn:aws:iam::{account_id}:role/cdk-hnb659fds-cfn-exec-role-{account_id}-ap-northeast-1",
             # ARN of the role used to look up context information in an environment
-            lookup_role_arn=f"arn:aws:iam::{env.AWS_ACCOUNT_ID}:role/cdk-hnb659fds-lookup-role-{env.AWS_ACCOUNT_ID}-ap-northeast-1"
+            lookup_role_arn=f"arn:aws:iam::{account_id}:role/cdk-hnb659fds-lookup-role-{account_id}-ap-northeast-1"
 )
 
 DEProjStack(
@@ -32,4 +32,5 @@ DEProjStack(
     synthesizer=project_synth,
     env=deploy_env
 )
+
 
